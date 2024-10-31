148b4445e00ad3040f6cdeb858b27c9dac2a4bec
Kenneth Looney <kenneth.looney@outlook.com>
2024-10-31 06:55:22 -0700

    refactor boot and loader assembly files; remove tasks.json and enhance string printing

--- projects/loader/loader.asm
+++ projects/loader/loader.asm
@@ -1,6 +1,25 @@
 [org 0x7e00]
 [bits 16]
 
-jmp $
+%define ENDL 0x0A, 0x0D
 
+start:
+    mov si, TestString
+    call print_string
+    jmp $ 
+print_string:
+    pusha              ; Save registers
+    mov ah, 0x0e       ; BIOS teletype function
+.next_char:
+    lodsb              ; Load next byte from string into AL
+    cmp al, 0          ; Check if end of string (null terminator)
+    je .done           ; If end of string, jump to done
+    int 0x10           ; Print character in AL
+    jmp .next_char     ; Repeat for next character
+.done:
+    popa               ; Restore registers
+    ret
+
+TestString: db "Booting up..", ENDL, 0
+times 2048-($-$$) db 0
  
