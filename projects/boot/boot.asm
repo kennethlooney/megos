f9b34970d108007a5c55108f6ded63a0c41282af
Kenneth Looney <kenneth.looney@outlook.com>
2024-10-31 05:14:53 -0700

    add "Hello World!" output to boot.asm

--- projects/boot/boot.asm
+++ projects/boot/boot.asm
@@ -12,6 +12,21 @@ mov al, 'l'
 int 0x10
 mov al, 'o'
 int 0x10
+mov al, ' '
+int 0x10
+mov al, 'W'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'd'
+int 0x10
+mov al, '!'
+int 0x10
+
 
 
 jmp $
