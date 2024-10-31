2356593e12c94ca70be96f56532a270d344496c4
Kenneth Looney <kenneth.looney@outlook.com>
2024-10-31 14:05:16 -0700

    Now in 64bit mode

--- boot/loader.asm
+++ boot/loader.asm
@@ -12,6 +12,9 @@ start:
 
 %include 'print.asm'
 %include 'gdt.asm'
+
+
+
 EnterProtectedMode:
     call EnableA20      ; Enable A20 line
     cli                 ; Disable interrupts
@@ -20,12 +23,42 @@ EnterProtectedMode:
     or eax, 1
     mov cr0, eax
 
-    jmp $
+    jmp codeseg:StartProtectedMode
 
 EnableA20:
     in al, 0x92
     or al, 2
     out 0x92, al
     ret
+
+[bits 32]
+%include 'cpuid.asm'
+%include 'simplepaging.asm'
+StartProtectedMode:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    
+    call DetectCPUID
+    call DetectLongMode
+    call SetupIdentityPaging
+    call EditGDT
+    jmp codeseg:Start64Bit
+
+
+ [bits 64]
+ Start64Bit:
+    mov edi, 0xb8000
+    mov rax, 0x1f201f201f201f20
+    mov ecx, 500
+    rep stosq
+    jmp $
+
+ 
+ 
 times 2048-($-$$) db 0
+
  
---
+++ boot/simplepaging.asm
@@ -0,0 +1,45 @@
+PageTableEntry: equ 0x1000
+SetupIdentityPaging:
+    push eax
+    push ebx
+    push ecx
+    push edx
+    push edi
+    mov edi, PageTableEntry
+    mov cr3, edi
+    mov dword[edi], 0x2003
+    add edi, 0x1000
+    mov dword[edi], 0x3003
+    add edi, 0x1000
+    mov dword[edi], 0x4003
+    add edi, 0x1000
+
+    mov ebx, 0x00000003
+    mov ecx, 512
+    .SetEntry:
+        mov dword[edi], ebx
+        add edi, 8
+        add ebx, 0x1000
+        loop .SetEntry
+
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    mov ecx, 0xC0000080
+    rdmsr
+    or  eax, (1 << 8)
+    wrmsr
+
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+
+    pop edi
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+
+    ret
